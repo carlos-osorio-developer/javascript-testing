3ce1f355cfec385630a246ce1d5a19c78c14f593
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-20 11:55:24 -0500

    Add first test

---
+++ functions.js
@@ -0,0 +1,7 @@
+const functs = {
+  stringLength(string) {
+      return string.length;
+  }
+}
+
+export default functs;
---
+++ functions.test.js
@@ -0,0 +1,5 @@
+import functs from "./functions";
+
+test('stringLength of Wharever to equal 8', () => {
+  expect(functs.stringLength('Wharever')).toBe(8);
+});
