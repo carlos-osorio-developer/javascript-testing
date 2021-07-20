9586103cc7355a5279e4d1daedb8f23a3e17d5c0
carlos-osorio-developer <osorio.uis@gmail.com>
2021-07-20 12:12:09 -0500

    Add example of how to throw an ERROR and how to test it

--- functions.js
+++ functions.js
@@ -1,6 +1,11 @@
 const functs = {
   stringLength(string) {
+    if (string.length < 1 || string.length > 10) {
+      throw new Error('Your string can not be less than 1 or bigger than 10 chars');
+    }
+    else {
       return string.length;
+    }
   }
 }
 
--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ import functs from "./functions";
 test('stringLength of Wharever to equal 8', () => {
   expect(functs.stringLength('Wharever')).toBe(8);
 });
+
+test('stringLength of empty string returns an error', () => {
+  expect(() => functs.stringLength('')).toThrow(Error);
+});
